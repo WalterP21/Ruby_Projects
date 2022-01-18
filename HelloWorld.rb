8caac5000ec11ef545ab27dcea37e5f8717f15dc
Walter Porzel <walterporzel@Walters-MacBook-Pro.local>
2022-01-18 17:03:11 -0500

    First Commit

---
+++ HelloWorld.rb
@@ -0,0 +1,16 @@
+# Hello world
+puts "Hello World"
+# Doesn't return nil
+p "Hello World"
+# No new line after
+print "Hello World"
+
+# variable
+greeting = "Hello world"
+p "Hello World"
+
+# Defining a method
+def say_hello(input)
+    puts "Hello " + input
+end
+say_hello("Walter")
